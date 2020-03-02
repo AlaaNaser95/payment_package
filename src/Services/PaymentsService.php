aa1bd50ef3da311551f73531fb29fb0384a8cbac
AlaaNaser95 <alaanaser95.95@gmail.com>
2020-03-02 16:57:54 +0300

    limitting getAllPaymentsMethod to return only paied ones

--- src/Services/PaymentsService.php
+++ src/Services/PaymentsService.php
@@ -3,24 +3,22 @@
 
 namespace beinmedia\payment\Services;
 
-
-use beinmedia\payment\models\MyFatoorah;
 use Illuminate\Support\Facades\DB;
 
 class PaymentsService
 {
     public function getMyFatoorahPayments(){
-        return DB::table('myfatoorah_payments')->select('payment_id','invoice_value as amount','currency','invoice_status as status', 'created_at','payment_method')->addSelect(DB::raw("'my_fatoorah' as service"));
+        return DB::table('myfatoorah_payments')->select('payment_id','invoice_value as amount','currency','invoice_status as status', 'created_at','payment_method')->where('invoice_status','Paid')->addSelect(DB::raw("'my_fatoorah' as service"));
     }
     public function getTapPayments(){
-        return DB::table('tap_payments')->select('charge_id as payment_id', 'amount','currency','status','created_at','source_id as payment_method')->addSelect(DB::raw("'tap' as service"));
+        return DB::table('tap_payments')->select('charge_id as payment_id', 'amount','currency','status','created_at','source_id as payment_method')->where('status','CAPTURED')->addSelect(DB::raw("'tap' as service"));
     }
     public function getPaypalPayments(){
-        return DB::table('paypal_payments')->select('payment_id','amount','currency','state as status','created_at','type as payment_method')->addSelect(DB::raw("'paypal' as service"));
+        return DB::table('paypal_payments')->select('payment_id','amount','currency','state as status','created_at','type as payment_method')->where('state','approved')->addSelect(DB::raw("'paypal' as service"));
     }
     public function getRecurringPayments(){
 
-        return DB::table('recurring_payments')->select('pay_id as payment_id','amount','currency','state as status','created_at')->addSelect(DB::raw("'paypal' as payment_method"))->addSelect(DB::raw("'paypal_recurring' as service"));
+        return DB::table('recurring_payments')->select('pay_id as payment_id','amount','currency','state as status','created_at')->where('state','completed')->addSelect(DB::raw("'paypal' as payment_method"))->addSelect(DB::raw("'paypal_recurring' as service"));
     }
     public function getAllPayments(){
         return $this->getMyFatoorahPayments()
